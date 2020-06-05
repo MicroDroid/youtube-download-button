c013ed2dc4cc099fca82e14aee2b2c1cd626ed19
Javier López Medina <jlmedina.tfe.92@gmail.com>
2020-06-05 13:47:19 +0100

    extraHeaders cannot be used in Firefox Detect if the browser is not Firefox. If that's the case, don't add extraHeaders. In another case, add it.

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,8 @@
 
+//If is not the Firefox browser, add the extraHeaders to the array
+let isFirefox = typeof InstallTrigger !== 'undefined';
+let extraInfoSpec = !isFirefox ? ['blocking', 'requestHeaders', 'extraHeaders'] : ['blocking', 'requestHeaders'];
+
 chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
     let header = details.requestHeaders.find(h => h.name === 'Cookie');
     if (header) { header.value = ''; }
@@ -9,4 +13,4 @@ chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
       'https://www.youtube.com/get_video_info*'
     ],
     types: ['xmlhttprequest'],
-  }, ['blocking', 'requestHeaders', "extraHeaders"]);
+  }, extraInfoSpec);
